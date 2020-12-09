a3d6406b6cbd6aff8aa82728ceb20122ea99831d
nyuta <pillowforneedles@gmail.com>
2020-12-09 05:04:49 +0200

    styles

--- src/components/employees/EmployyesStyled.js
+++ src/components/employees/EmployyesStyled.js
@@ -3,12 +3,18 @@ import styled from 'styled-components';
 export const EmployeesStyled = styled.div`
   display: flex;
   justify-content: space-evenly;
-  list-style: none;
   padding: 30px;
 
   li {
     display: flex;
     justify-content: space-between;
-    width: 130px;
+    width: 250px;
+  }
+
+  input[type='checkbox']:checked,
+  input[type='checkbox']:not(:checked) {
+    width: 10px;
+    height: 10px;
+    cursor: pointer;
   }
 `;
